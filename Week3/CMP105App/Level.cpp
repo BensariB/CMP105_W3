15c43f76554c93262de36acd0e4d01a031e0c886
1903102 <1903102@uad.ac.uk>
2020-01-30 10:40:09 +0000

    Task 4-5

--- Week3/CMP105App/Level.cpp
+++ Week3/CMP105App/Level.cpp
@@ -12,8 +12,8 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	circle.setPosition(100, 100);
 	circle.setOutlineColor(sf::Color::White);
 	circle.setOutlineThickness(2.f);
-	speed = sf::Vector2f(360.f, 100);
-	direction = sf::Vector2f (1.f, 0.5f);
+	speed = sf::Vector2f(360.f, 360.f);
+	direction = sf::Vector2f (1.f, 1.f);
 }
 
 Level::~Level()
@@ -24,26 +24,56 @@ Level::~Level()
 // handle user input
 void Level::handleInput(float dt)
 {
+	// {
+		if (input->isKeyDown(sf::Keyboard::Right)) {
+			direction.x = 1.f;
+		}
+		else if (input->isKeyDown(sf::Keyboard::Left)) {
+			direction.x = -1.f;
+		}
+		else {
+			direction.x = 0.f;
+		}
+	
 
+	//if (circle.getPosition().y + circle.getRadius() *2 < window->getSize().y && circle.getPosition().y - circle.getRadius() * 2 > 0) {
+		if (input->isKeyDown(sf::Keyboard::Up)) {
+			direction.y = -1.f;
+		}
+		else if (input->isKeyDown(sf::Keyboard::Down)) {
+			direction.y = 1.f;
+		}
+		else {
+			direction.y = 0.f;
+		}
+	//}
+	//else {
+		//direction.y = 0.f;
+	//}
+	
+	
+	
 }
 
 // Update game objects
 void Level::update(float dt)
 {
-	circle.move(speed.x * direction.x * dt, speed.y * direction.y * dt);
-
-	if (circle.getPosition().x + circle.getRadius() > window->getSize().x ) {
+	
+	
+	if (circle.getPosition().x + circle.getRadius() >= window->getSize().x ) {
 		direction.x *= -1;
 	}
 	else if (circle.getPosition().x - circle.getRadius() < 0) {
 		direction.x *= -1;
 	}
-	if (circle.getPosition().y + circle.getRadius() > window->getSize().y) {
+	if (circle.getPosition().y + circle.getRadius() >= window->getSize().y) {
 		direction.y *= -1;
 	}
 	else if (circle.getPosition().y - circle.getRadius() < 0) {
 		direction.y *= -1;
 	}
+	circle.move(speed.x * direction.x * dt, speed.y * direction.y * dt);
+	
 
 }
 
