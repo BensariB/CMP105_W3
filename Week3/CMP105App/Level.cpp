baad9590fab26e88832ebd56fc6cbe65e7283469
1903102 <1903102@uad.ac.uk>
2020-01-30 11:00:54 +0000

    Week 3 tasks

--- Week3/CMP105App/Level.cpp
+++ Week3/CMP105App/Level.cpp
@@ -12,7 +12,7 @@ Level::Level(sf::RenderWindow* hwnd, Input* in)
 	circle.setPosition(100, 100);
 	circle.setOutlineColor(sf::Color::White);
 	circle.setOutlineThickness(2.f);
-	speed = sf::Vector2f(360.f, 360.f);
+	speed = sf::Vector2f(200.f, 200.f);
 	direction = sf::Vector2f (1.f, 1.f);
 }
 
@@ -23,58 +23,50 @@ Level::~Level()
 
 // handle user input
 void Level::handleInput(float dt)
-{
-	// {
-		if (input->isKeyDown(sf::Keyboard::Right)) {
+		/*if (input->isKeyDown(sf::Keyboard::Right)) {
 			direction.x = 1.f;
+			input->setKeyUp(sf::Keyboard::Right);
 		}
 		else if (input->isKeyDown(sf::Keyboard::Left)) {
 			direction.x = -1.f;
+			input->setKeyUp(sf::Keyboard::Left);
 		}
 		else {
 			direction.x = 0.f;
+		}*/
+{
+		if (input->isKeyDown(sf::Keyboard::LShift)) {
+			speed = sf::Vector2f(400.f, 400.f);
+			//input->setKeyUp(sf::Keyboard::LShift);
 		}
-	
-
-	//if (circle.getPosition().y + circle.getRadius() *2 < window->getSize().y && circle.getPosition().y - circle.getRadius() * 2 > 0) {
-		if (input->isKeyDown(sf::Keyboard::Up)) {
-			direction.y = -1.f;
-		}
-		else if (input->isKeyDown(sf::Keyboard::Down)) {
-			direction.y = 1.f;
+		else if (input->isKeyDown(sf::Keyboard::BackSpace)) {
+			speed = sf::Vector2f(100.f, 100.f);
+			//input->setKeyUp(sf::Keyboard::BackSpace);
 		}
 		else {
-			direction.y = 0.f;
+			speed = sf::Vector2f(200.f, 200.f);
 		}
-	//}
-	//else {
-		//direction.y = 0.f;
-	//}
-	
-	
-	
+			
 }
 
 // Update game objects
 void Level::update(float dt)
 {
 	
-	
-	if (circle.getPosition().x + circle.getRadius() >= window->getSize().x ) {
-		direction.x *= -1;
-	}
-	else if (circle.getPosition().x - circle.getRadius() < 0) {
-		direction.x *= -1;
-	}
-	if (circle.getPosition().y + circle.getRadius() >= window->getSize().y) {
-		direction.y *= -1;
-	}
-	else if (circle.getPosition().y - circle.getRadius() < 0) {
-		direction.y *= -1;
-	}
 	circle.move(speed.x * direction.x * dt, speed.y * direction.y * dt);
+	if (circle.getPosition().x + circle.getRadius() * 2 > window->getSize().x ) {
+		direction.x *= -1;
+	}
+	else if (circle.getPosition().x < 0) {
+		direction.x *= -1;
+	}
+	if (circle.getPosition().y + circle.getRadius() * 2 > window->getSize().y) {
+		direction.y *= -1;
+	}
+	else if (circle.getPosition().y< 0) {
+		direction.y *= -1;
+	}
 	
-
 }
 
 // Render level
